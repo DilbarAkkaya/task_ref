b9c50106f56fdf6236f4c074bca95fcfea7d2baf
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-10-27 18:06:26 +0300

    feat: add react-bootstrap, and form

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {Container} from 'react-bootstrap';
 import './App.css';
 
+class Form extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render(){
+    return (
+      <Container>
+        <form className='w-50 border mt-5 p-3 m-auto'>
+          <div className='mb-3'>
+            <label htmlFor='exampleFormControlInput1' className='form-label'>Email address</label>
+            <input type='email' className='form-control' id='exampleFormControlInput1'
+            placeholder='name@example.com'/>
+          </div>
+          <div className='mb-3'>
+            <label htmlFor='exampleFormControlTextarea1' className='form-label'>Example textarea</label>
+          <textarea className='form-control' id='exampleFormControlTextarea1' rows='3'></textarea>
+          </div>
+        </form>
+      </Container>
+    )
+  }
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Form/>
   );
 }
 
