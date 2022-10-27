dc286a0061a498a15d0b0f93cfcfd86e53a9be71
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-10-27 23:37:54 +0300

    feat: add method for redirect ref

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -7,7 +7,10 @@ class Form extends Component {
     super(props);
     this.myRef = React.createRef();
   }
-  componentDidMount(){
+/*   componentDidMount(){
+    this.myRef.current.focus();
+  } */
+  focusFirstTI = () => {
     this.myRef.current.focus();
   }
   render(){
@@ -21,7 +24,7 @@ class Form extends Component {
           </div>
           <div className='mb-3'>
             <label htmlFor='exampleFormControlTextarea1' className='form-label'>Example textarea</label>
-          <textarea className='form-control' id='exampleFormControlTextarea1' rows='3'></textarea>
+          <textarea onClick={this.focusFirstTI} className='form-control' id='exampleFormControlTextarea1' rows='3'></textarea>
           </div>
         </form>
       </Container>
