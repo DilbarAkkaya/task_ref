6544ba0c75d352729ed9f5a1973dd9bff8c41f7c
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-10-28 00:14:59 +0300

    feat: add portal component

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,13 +1,14 @@
-import React, {Component} from 'react';
-import {Container} from 'react-bootstrap';
+import React, { Component } from 'react';
+import  ReactDOM  from 'react-dom';
+import { Container } from 'react-bootstrap';
 import './App.css';
 
 class Form extends Component {
-    //myRef = React.createRef();
+  //myRef = React.createRef();
 
-/*   componentDidMount(){
-    this.myRef.current.focus();
-  } */
+  /*   componentDidMount(){
+      this.myRef.current.focus();
+    } */
 
   setInputRef = elem => {
     this.myRef = elem;
@@ -17,27 +18,52 @@ class Form extends Component {
       this.myRef.focus();
     }
   }
-  render(){
+  render() {
     return (
       <Container>
         <form className='w-50 border mt-5 p-3 m-auto'>
           <div className='mb-3'>
             <label htmlFor='exampleFormControlInput1' className='form-label'>Email address</label>
             <input ref={this.setInputRef} type='email' className='form-control' id='exampleFormControlInput1'
-            placeholder='name@example.com'/>
+              placeholder='name@example.com' />
           </div>
           <div className='mb-3'>
             <label htmlFor='exampleFormControlTextarea1' className='form-label'>Example textarea</label>
-          <textarea onClick={this.focusFirstTI} className='form-control' id='exampleFormControlTextarea1' rows='3'></textarea>
+            <textarea onClick={this.focusFirstTI} className='form-control' id='exampleFormControlTextarea1' rows='3'></textarea>
           </div>
+          <Portal>
+            <Msg />
+          </Portal>
         </form>
       </Container>
     )
   }
 }
+
+const Portal = (props) => {
+  const node = document.createElement('div');
+  document.body.appendChild(node);
+
+  return ReactDOM.createPortal(props.children, node);
+}
+
+const Msg = () => {
+  return (
+    <div style={{
+      'width': '500px',
+      'height': '150px',
+      'backgroundColor': 'red',
+      'position': 'absolute',
+      'right': '0',
+      'bottom': '0'
+    }}>
+
+    </div>
+  )
+}
 function App() {
   return (
-    <Form/>
+    <Form />
   );
 }
 
